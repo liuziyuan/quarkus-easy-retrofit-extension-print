3805ae58799456570ef9c431d912dab89a493ba8
liuziyuan <liuziyuan.room@gmail.com>
2024-09-26 15:16:35 +0800

    update

--- runtime/src/main/java/io/github/liuziyuan/quarkus/easy/retrofit/extension/print/runtime/PrintExtension.java
+++ runtime/src/main/java/io/github/liuziyuan/quarkus/easy/retrofit/extension/print/runtime/PrintExtension.java
@@ -1,9 +1,9 @@
 package io.github.liuziyuan.quarkus.easy.retrofit.extension.print.runtime;
 
 import io.github.easyretrofit.core.RetrofitInterceptorExtension;
+import io.github.easyretrofit.core.delegate.BaseExceptionDelegate;
 import io.github.easyretrofit.core.exception.RetrofitExtensionException;
 import io.github.easyretrofit.core.extension.BaseInterceptor;
-import io.github.easyretrofit.core.proxy.BaseExceptionDelegate;
 
 import java.lang.annotation.Annotation;
 
